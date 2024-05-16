4d35cc5914c17dc2fd17415faa22c84b256a5523
soollan <soollan6@gmail.com>
2024-05-16 16:25:48 +0900

    init

---
+++ src/main/java/com/musinsa/mycoordinator/MyCoordinatorApplication.java
@@ -0,0 +1,13 @@
+package com.musinsa.mycoordinator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCoordinatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCoordinatorApplication.class, args);
+    }
+
+}
